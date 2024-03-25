894270369286a8c2e12cd0666939ad2848b27737
Esther-Naserian <naseriansamuria@gmail.com>
2024-03-25 08:29:24 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,51 @@
+fun main(){
+    myNumbers(arrayOf(12,43,89,))
+    myNames(arrayOf("Barbie bakers brown bagels and buns"))
+    calculate(314159*20)
+
+
+}
+fun myNames(names:Array<String>) {
+    println("Barbie bakers brown bagels  and buns")
+    println(names.forEach {"b"
+
+        println("b")
+
+    })
+    println(names.contentToString())
+
+
+
+
+}
+class myNumbers{
+
+}
+
+
+}
+fun myNumbers(num:Array<Int>):Int{
+    println(num.contentToString())
+    println(num.sum())
+    println(num.count())
+    println(num.average())
+    println(num.contentToString())
+}
+fun calculate(area: Double): Int{
+    println(area)
+}
+    fun ispalindrome(word: String):Boolean{
+        var
+        return true
+        return false
+
+
+
+
+
+
+
+}
+
+
+
